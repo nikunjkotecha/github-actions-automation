ed4370d34883e21f83e6f5cecebb4ace30a9239a
Nikunj Kotecha <nikunj.kotecha@acquia.com>
2022-01-23 15:55:24 +0530

    GAB-00: Use everything custom with Docker.

--- blt/scripts/github-actions/setup_env.sh
+++ blt/scripts/github-actions/setup_env.sh
@@ -3,6 +3,7 @@
 set -ev
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt -y update
 apt -y -f install ./google-chrome-stable_current_amd64.deb
 rm google-chrome-*.deb
 
