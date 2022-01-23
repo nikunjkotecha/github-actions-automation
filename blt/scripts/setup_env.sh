e9fa41b0970506a0824fe734052970c344d186c6
Nikunj Kotecha <nikunj.kotecha@acquia.com>
2022-01-23 11:58:27 +0530

    GAB-00: Use Selenoid Server github action.

---
+++ blt/scripts/setup_env.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -ev
+
+# Create a MySQL database for drupal to use
+MYSQL_ROOT_COMMAND="mysql --user=root --password=$MYSQL_ROOT_PASSWORD --host=$MYSQL_HOST --protocol=tcp"
+echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE_NAME\`;" | $MYSQL_ROOT_COMMAND
+echo "CREATE USER '$MYSQL_USER_NAME'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';" | $MYSQL_ROOT_COMMAND
+echo "GRANT ALL ON $MYSQL_DATABASE_NAME.* TO '$MYSQL_USER_NAME'@'%';" | $MYSQL_ROOT_COMMAND
+echo "FLUSH PRIVILEGES;" | $MYSQL_ROOT_COMMAND
+
+# Print databases which drupal user can see for debugging
+echo "SHOW DATABASES;" | mysql --user="$MYSQL_USER_NAME" --password="$MYSQL_USER_PASSWORD" --host="$MYSQL_HOST" --protocol=tcp
+
+set +v
