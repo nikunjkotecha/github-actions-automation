807178c0a7f85811c6ed239e24a6435c8160f22e
Nikunj Kotecha <nikunj.kotecha@acquia.com>
2022-01-23 14:51:40 +0530

    GAB-00: Use everything custom with Docker.

--- blt/scripts/github-actions/setup_env.sh
+++ blt/scripts/github-actions/setup_env.sh
@@ -2,6 +2,27 @@
 
 set -ev
 
+export PATH=${COMPOSER_BIN}:$PATH
+
+# Prepare ssh config for deployment to Acquia Cloud.
+mkdir -p ~/.ssh
+touch ~/.ssh/config
+chmod 600 ~/.ssh/config
+# Trust all Acquia git/svn hosts.
+printf "Host *.enterprise-g1.hosting.acquia.com\n  StrictHostKeyChecking no\n" >> ~/.ssh/config
+# Github actions will run steps as root.
+# Related to https://github.community/t/how-to-run-action-as-a-non-root-user/17572
+mkdir -p /root/.ssh
+cp ~/.ssh/config /root/.ssh/config
+
+# Set the git configuration
+git config --global user.name "github-actions-CI"
+git config --global user.email "noreply@github.com"
+
+# Up the PHP Memory Limit
+touch /usr/local/etc/php/conf.d/docker-php-ext-ci.ini
+echo 'memory_limit = -1' >> /usr/local/etc/php/conf.d/docker-php-ext-ci.ini
+
 # Create a MySQL database for drupal to use
 MYSQL_ROOT_COMMAND="mysql --user=root --password=$MYSQL_ROOT_PASSWORD --host=$MYSQL_HOST --protocol=tcp"
 echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE_NAME\`;" | $MYSQL_ROOT_COMMAND
