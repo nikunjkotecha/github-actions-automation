c68a0861f4ac05a1b3434bd792e0d1b4ce7f0799
Nikunj Kotecha <nikunj.kotecha@acquia.com>
2022-09-29 01:42:44 +0530

    Add tests with Pa11y.

--- blt/scripts/github-actions/setup_env.sh
+++ blt/scripts/github-actions/setup_env.sh
@@ -15,6 +15,7 @@ touch ~/.ssh/config
 chmod 600 ~/.ssh/config
 # Trust all Acquia git/svn hosts.
 printf "Host *.enterprise-g1.hosting.acquia.com\n  StrictHostKeyChecking no\n" >> ~/.ssh/config
+
 # Github actions will run steps as root.
 # Related to https://github.community/t/how-to-run-action-as-a-non-root-user/17572
 mkdir -p /root/.ssh
@@ -23,6 +24,8 @@ cp ~/.ssh/config /root/.ssh/config
 # Set the git configuration
 git config --global user.name "github-actions-CI"
 git config --global user.email "noreply@github.com"
+
+blt blt:telemetry:disable --no-interaction
 
 # Up the PHP Memory Limit
 touch /usr/local/etc/php/conf.d/docker-php-ext-ci.ini
--- blt/scripts/github-actions/test_app.sh
+++ blt/scripts/github-actions/test_app.sh
@@ -5,5 +5,6 @@ set -ev
 cd ${GITHUB_WORKSPACE}
 
 $BLT_DIR/bin/blt tests:behat:run -D tests.run-server=true --no-interaction --ansi -vvv
+$BLT_DIR/bin/blt tests:pa11y:run -D tests.run-server=true --no-interaction --ansi -vvv
 
 set +v
