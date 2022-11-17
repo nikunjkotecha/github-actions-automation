469edd97baabbf8615490b4cbf8932954abf6d1f
Nikunj Kotecha <nikunj.kotecha@acquia.com>
2022-11-17 23:14:05 +0530

    GAA-00: Test without docker.

--- blt/scripts/github-actions/setup_app.sh
+++ blt/scripts/github-actions/setup_app.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-${BLT_DIR}/bin/blt setup --define drush.alias='${drush.aliases.ci}' --environment=ci --no-interaction --ansi --verbose
+blt setup --define drush.alias='${drush.aliases.ci}' --environment=ci --no-interaction --ansi --verbose
 
 cd "$GITHUB_WORKSPACE/docroot"
 drush pm:enable clientside_validation_jquery
--- blt/scripts/github-actions/setup_env.sh
+++ blt/scripts/github-actions/setup_env.sh
@@ -4,27 +4,7 @@ set -ev
 
 export PATH=${COMPOSER_BIN}:$PATH
 
-# Prepare ssh config for deployment to Acquia Cloud.
-mkdir -p ~/.ssh
-touch ~/.ssh/config
-chmod 600 ~/.ssh/config
-# Trust all Acquia git/svn hosts.
-printf "Host *.enterprise-g1.hosting.acquia.com\n  StrictHostKeyChecking no\n" >> ~/.ssh/config
-
-# Github actions will run steps as root.
-# Related to https://github.community/t/how-to-run-action-as-a-non-root-user/17572
-mkdir -p /root/.ssh
-cp ~/.ssh/config /root/.ssh/config
-
-# Set the git configuration
-git config --global user.name "github-actions-CI"
-git config --global user.email "noreply@github.com"
-
 blt blt:telemetry:disable --no-interaction
-
-# Up the PHP Memory Limit
-touch /usr/local/etc/php/conf.d/docker-php-ext-ci.ini
-echo 'memory_limit = -1' >> /usr/local/etc/php/conf.d/docker-php-ext-ci.ini
 
 # Start MySQL
 sudo systemctl start mysql.service
