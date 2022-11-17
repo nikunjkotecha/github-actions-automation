ca0ae3ebab403716c6e2d967c7dea73caa870afe
Nikunj Kotecha <nikunj.kotecha@acquia.com>
2022-11-17 22:54:15 +0530

    GAA-0: Add missing test scenario.

--- tests/behat/features/bootstrap/Drupal/FeatureContext.php
+++ tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -19,4 +19,29 @@ class FeatureContext extends RawDrupalContext {
 
   }
 
+  /**
+   * Wait for page to load for 15 seconds.
+   *
+   * @Given /^I wait for the page to load$/
+   */
+  public function iWaitForThePageToLoad() {
+    $this->getSession()->wait(60000, "document.readyState === 'complete'");
+  }
+
+  /**
+   * @When /^I click on "([^"]*)" element$/
+   */
+  public function iClickOnElement($css_selector) {
+    $session = $this->getSession();
+
+    $element = $session->getPage()->find("css", $css_selector);
+
+    if ($element) {
+      $element->click();
+    }
+    else {
+      throw new Exception("Element $css_selector not found on " . $session->getCurrentUrl());
+    }
+  }
+
 }
