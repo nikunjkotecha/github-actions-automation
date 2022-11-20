c8a9da8f07f3c1871856633c3d610672fb269084
Nikunj Kotecha <nikunj.kotecha@acquia.com>
2022-11-20 11:16:00 +0530

    GAA-00: Init PA11Y and do frontend setup.

--- blt/scripts/github-actions/test_pa11y.sh
+++ blt/scripts/github-actions/test_pa11y.sh
@@ -4,6 +4,8 @@ set -ev
 
 cd ${GITHUB_WORKSPACE}
 
+blt frontend:setup --no-interaction --ansi -vvv
+blt tests:pa11y:init --no-interaction --ansi -vvv
 blt tests:pa11y:run -D tests.run-server=true --no-interaction --ansi -vvv
 
 set +v
